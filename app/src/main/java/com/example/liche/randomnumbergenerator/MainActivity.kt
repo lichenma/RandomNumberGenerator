f313e9abb0ad49f32c363180dfc0c1472d187fd3
Lichen Ma <lichenma123@gmail.com>
2019-07-21 14:42:10 -0400

    working version

--- app/src/main/java/com/example/liche/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/liche/randomnumbergenerator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.liche.randomnumbergenerator
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,6 +17,7 @@ class MainActivity : AppCompatActivity() {
      * Show a toast
      * @param view - the view that is clicked
      */
+    private var TOTAL_COUNT = "total_count";
     public fun toastMe(view: View){
         val myToast: Toast = Toast.makeText(this, "Hello Toast!", Toast.LENGTH_SHORT);
         myToast.show();
@@ -24,8 +26,17 @@ class MainActivity : AppCompatActivity() {
     public fun countMe(view: View){
         var showCountTextView: TextView = findViewById(R.id.randomNumber) as TextView;
         var countString: String = showCountTextView.getText().toString();
-        var count = Integer.parseInt(countString);
+        var count:Int = Integer.parseInt(countString);
         count++;
         showCountTextView.setText(count.toString());
+    }
+
+    public fun randomMe(view: View){
+        var randomIntent = Intent(this, SecondActivity::class.java)
+        var showCountTextView:TextView = findViewById(R.id.randomNumber) as TextView;
+        var countString:String = showCountTextView.getText().toString();
+        var count:Int = Integer.parseInt(countString);
+        randomIntent.putExtra(TOTAL_COUNT, count);
+        startActivity(randomIntent)
     }
 }
--- app/src/main/java/com/example/liche/randomnumbergenerator/SecondActivity.kt
+++ app/src/main/java/com/example/liche/randomnumbergenerator/SecondActivity.kt
@@ -2,11 +2,29 @@ package com.example.liche.randomnumbergenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.util.*
 
 class SecondActivity : AppCompatActivity() {
-
+    private var TOTAL_COUNT = "total_count";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        showRandomNumber()
     }
+    public fun showRandomNumber(){
+        var randomView:TextView = findViewById(R.id.textView3) as TextView
+        var headingView:TextView = findViewById(R.id.textView) as TextView
+        var count:Int = getIntent().getIntExtra(TOTAL_COUNT,0)
+        var random:Random = Random();
+        var randomInt:Int = 0;
+        if (count>0){
+            randomInt = random.nextInt(count);
+        }
+
+        randomView.setText(Integer.toString(randomInt));
+        headingView.setText(getString(R.string.random_heading,count))
+    }
+
 }
